f016188b4323281b0556758afbca7cf98c360a17
buithebinh1998 <buithebinh1998@gmail.com>
2019-12-13 22:54:38 +0700

    add visible commit

--- src/component/Courses/LearnBasic/VideoCourse/VideoCourse.js
+++ src/component/Courses/LearnBasic/VideoCourse/VideoCourse.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Document, Page } from "react-pdf";
 import './VideoCourse.scss'
+import pre from '../../../../assets/icon/chevron-right.svg'
 
+import next from '../../../../assets/icon/chevron-left.svg'
 import pdfFile from '../../../../assets/pdf/Bai1.pdf'
 
 const options = {
@@ -10,31 +12,48 @@ const options = {
 }
 
 class VideoCourse extends React.Component {
-  state = { numPages: null, pageNumber: 1 };
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      numPages: null,
+      pageNumber: 1,
+      visibleLeft: '',
+      visibleRight: '',
+    }
+  }
 
   onDocumentLoadSuccess = ({ numPages }) => {
-    this.setState({ numPages });
-  };
+    this.setState({ numPages })
+  }
 
-  goToPrevPage = () =>
-    this.setState(state => ({ pageNumber: state.pageNumber - 1 }));
-  goToNextPage = () =>
-    this.setState(state => ({ pageNumber: state.pageNumber + 1 }));
+  goToPrevPage = () => {
 
+    if (this.state.pageNumber !== 1) {
+      this.setState(state => ({ pageNumber: state.pageNumber - 1 }))
+      this.setState({ visibleLeft: '', visibleRight: '' })
+    }
+    if (this.state.pageNumber === 1) {
+      this.setState({ visibleLeft: 'hide', visibleRight: '' })
+    }
 
+  }
+
+  goToNextPage = () => {
+    if (this.state.pageNumber !== this.state.numPages) {
+      this.setState(state => ({ pageNumber: state.pageNumber + 1 }));
+      this.setState({ visibleRight: '', visibleLeft: '' })
+    }
+    if (this.state.pageNumber === this.state.numPages) {
+      this.setState({ visibleRight: 'hide', visibleLeft: '' })
+    }
+  }
   render() {
-    const { pageNumber, numPages } = this.state;
+    const { pageNumber, numPages, visibleLeft, visibleRight } = this.state;
 
     return (
       <div className="video-background">
         <div className="Example__container">
-          <div className="Example__container__load">
-            <nav>
-              <button onClick={this.goToPrevPage}>Prev</button>
-              <button onClick={this.goToNextPage}>Next</button>
-            </nav>
-          </div>
+
           <div className="Example__container__document">
             <Document
               options={options}
@@ -44,10 +63,24 @@ class VideoCourse extends React.Component {
               <Page pageNumber={pageNumber} top='0' left='0' />
             </Document>
           </div>
+          <div className="Example__container__load">
+            <nav>
+              <div onClick={this.goToPrevPage} className={`${visibleLeft}`}>
+                <img alt='img' src={next} style={{ width: '40px' }} />
+              </div>
+
+              <div>
+                <p style={{ marginTop: '5px', fontWeight: 'bold' }}>
+                  Page {pageNumber} of {numPages}
+                </p>
+              </div>
+              <div onClick={this.goToNextPage} className={`${visibleRight}`}>
+                <img alt='img' src={pre} style={{ width: '40px' }} />
+              </div>
+            </nav>
+          </div>
         </div>
-        <p>
-          Page {pageNumber} of {numPages}
-        </p>
+
       </div>
     )
   }
